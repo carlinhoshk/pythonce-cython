661b16f59ecb527cc821846a835e0ce1f898e2ea
carlinhoshk <carlosmdohk@gmail.com>
2023-06-22 00:15:56 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from os.path import join
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+
+directory_path = os.path.dirname(
+    os.path.abspath(__file__)
+    )
+
+
+ext_data = {
+    'pythonce.ola.wrap_ola': {
+        'sources': [
+            join(directory_path, 'pythonce', 'ola', 'wrap_ola.pyx'),
+            join(directory_path, 'pythonce', 'ola', 'ola.c')]
+    }
+}
+
+
+extensions = []
+
+for name, data in ext_data.items():
+
+    sources = data['sources']
+    include = data.get('include', [])
+
+    obj = Extension(
+        name,
+        sources=sources,
+        include_dirs=include
+    )
+    
+    extensions.append(obj)
+
+
+# Use cythonize on the extension object.
+setup(
+    name='pythonce',
+    author='Carlinhoshk',
+    #package_dir={'pythonce': ''},
+    ext_modules=cythonize(extensions)
+    )
